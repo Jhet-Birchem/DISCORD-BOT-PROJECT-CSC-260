1afd45125b98256a7d3eec2e0651240e38a1eace
Jhet_Birchem <jhet.birchem@trojans.dsu.edu>
2023-04-29 15:04:57 -0600

    Followed a different tutorial to help me make a template for my project. This feels like it will be much better for using various classes.

---
+++ Bot/Drama-Bot-V2/Drama-Bot-V2/Bot.cs
@@ -0,0 +1,81 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drama_Bot_V2
+{
+    public class Bot
+    {
+        // The three main things needed to interact with Discord.
+        public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            // Declare an empty string variable to read in the config.json file.
+            //                                 V
+            // The config.json file contains the token that connects the executable
+            // to the Discord Bot, making it appear online and active. The config.json
+            // file also contains a designated prefix "#" for accessing bot commands.
+            //
+            var json = string.Empty;
+
+            // Then, read the config.json file in it's entirety.
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            // Deserialize the config.json file into the ConfigJSON format.
+            var configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+            // Set the token of the bot within the portion below.
+            var config = new DiscordConfiguration()
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                // If the connect is interrupted to the bot profile, automatically reconnect.
+                AutoReconnect = true
+            };
+
+            // Configure the Client with the "var config" detailed right about this.
+            Client = new DiscordClient(config);
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                // If the bot doesn't receive anything for 30 minutes, time itself out.
+                Timeout = TimeSpan.FromMinutes(30)
+            });
+
+            // Where commands are configured.
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                // Set the commands prefix from the config.json.
+                StringPrefixes = new string[] { configJson.Prefix },
+                // Allows the prefix to be used.
+                EnableMentionPrefix = true,
+                // Allows the bot to send direct messages to users.
+                EnableDms = true
+            };
+
+            // Allows the bot to actually come online/establish the connect to the executable.
+            await Client.ConnectAsync();
+            // Prevents the connection from timing out/giving up.
+            await Task.Delay(-1);
+        }
+
+        // Return the "Task Complete" for the client being ready for use,
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Bot/Drama-Bot-V2/Drama-Bot-V2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drama_Bot_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new instance of the bot class.
+            var bot = new Bot();
+            // Allows us to await the completion of an asynchronous task.
+            //
+            // NOTE*** The asynchronous nature of the program is important
+            // for the fact that many tasks are not handled on the local
+            // machine exclusively.
+            //
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
