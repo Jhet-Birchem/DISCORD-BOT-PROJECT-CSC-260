533c205fd5822dc9bfab01e38e1cb58ef3f4d67d
Jhet_Birchem <jhet.birchem@trojans.dsu.edu>
2023-05-01 14:07:47 -0600

    Added an embedded message command just for practice making commands. Next I plan to refine the drama command and remove unnecessary commands.

--- Bot/Drama-Bot-V2/Drama-Bot-V2/Commands/DramaCommands.cs
+++ Bot/Drama-Bot-V2/Drama-Bot-V2/Commands/DramaCommands.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace Drama_Bot_V2.Commands
     {
         // Declaring existance of the drama command.
         [Command("drama")]
+
         // Declaring a method of a command.
         // NOTE*** ctx represents which channel that the user that used the command.
         public async Task DramaVOne(CommandContext ctx)
@@ -23,6 +25,7 @@ namespace Drama_Bot_V2.Commands
             await ctx.Channel.SendMessageAsync("Your mom!");
         }
 
+
         // Declaring existance of an add command.
         [Command("add")]
 
@@ -31,6 +34,23 @@ namespace Drama_Bot_V2.Commands
         {
             int answer = number1 + number2;
             await ctx.Channel.SendMessageAsync(answer.ToString());
+        }
+
+
+        // Declaring the existance of an embedded message command.
+        [Command("embed")]
+
+        // Declaring a method of the embedded message command.
+        public async Task SendEmbedMessage (CommandContext ctx)
+        {
+            var embedMessage = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
+
+                .WithTitle("This is a Title")
+                .WithDescription("This is the description")
+                );
+
+            await ctx.Channel.SendMessageAsync(embedMessage);
         }
     }
 }
